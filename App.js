31a10dcfd135be4d09a9c0676ccf51a0f091d6dd
Ingrid Feitosa Santos <ingridfeitosasts@gmail.com>
2024-01-13 22:11:08 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,51 +1,21 @@
 
-import React, {useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
-import { keyboard } from '@testing-library/user-event/dist/keyboard'
-
-
-
-
+import Classe from './componentes/Classe.js'
 
 export default function App(){
 
 
-const [senha, setSenha] = useState()
-
-const handleChange=(x)=>{
-  return setSenha(x.target.value)
-
-}
-
-
-const gravar_senha=(keyPass, keyValue)=>{
-  localStorage.setItem(keyPass, keyValue)
-}
-
-const ver_senha =(keyPass)=>{
-  alert(localStorage.getItem(keyPass))
-}
-
-const deletar_senha=(keyPass)=>{
-  localStorage.removeItem(keyPass)
-
-}
+ 
 
  return (
     <>
-    <main className='main'>
-      <p>Definição de senha</p>
-    <input type='password' placeholder='Enter your password' onChange={handleChange}></input>
-    <button onClick={()=>gravar_senha('keyPass', senha)}>Gravar senha</button>
-    <button onClick={()=>ver_senha('keyPass', senha)}>Ver senha</button>
-    <button onClick={()=>deletar_senha('keyPass', senha)}>Deletar senha</button>
-    </main>
-   
-    
+    <h1> o componente de classe esta abaixo: </h1>
+    <Classe/>
+
     </>
     
  )
 
   
 }
-
---
+++ componentes/Classe.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class Classe extends React.Component{
+
+
+    render(){
+
+
+        return (
+
+            <>
+            <p> Este é o primero componente Classe </p>
+            
+            </>
+        )
+    }
+}export default Classe 
